00b6ab07775c3e1388b3272effb1cffbc967ce50
TingPing <tngpng@gmail.com>
2013-08-06 17:28:06 -0400

    Update misc.js

--- examples/misc.js
+++ examples/misc.js
@@ -1,4 +1,3 @@
-
 SCRIPT_NAME = "misc";
 SCRIPT_VER = "1";
 SCRIPT_DESC = "examples";
@@ -6,4 +5,4 @@ SCRIPT_DESC = "examples";
 if (nickcmp (get_info ('nick'), 'TingPing') == 0)
 	print ('\00320You are cool!');
 else
-	print (strip('\002\00319You are not cool, no colors for you.', 1));
+	print (strip('\002\00319You are not cool, no colors for you.', STRIP_COLOR));
